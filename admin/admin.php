4e9a79035d7b8be9358d5a27bfdbf5c4dfb774f5
Phil Tanner <phil@philtanner.com>
2016-08-31 07:27:21 +1200

    Menu items

--- admin/admin.php
+++ admin/admin.php
@@ -21,5 +21,44 @@
 	require_once( dirname(__FILE__).'/../class/defines.php' );
 	
 	// First of all, we need to create a new top level menu
+	// Taken from:https://codex.wordpress.org/Adding_Administration_Menus
 	
+	add_action( 'admin_menu', 'nzcf_cadet_net_plugin_menu' );
+	
+	function nzcf_cadet_net_plugin_menu(){
+		// Add a new main menu level for CadetNet Admin
+		add_menu_page( 
+			__("NZCF CadetNet", 'nzcf-cadet-net'), // Page title
+			__("CadetNet", 'nzcf-cadet-net'),       // Menu text
+			"manage_options", // Capability required (Needed to save option changes to system)
+			"cadet_net_menu", // Menu slug (unique name)
+			"cadetnet_admin_menu" //, // Function to be called when displaying content
+			//$icon_url, // The url to the icon to be used for this menu. This parameter is optional.
+		);
+		
+		// Add sub menu items:
+		add_submenu_page( 
+			"cadet_net_menu", 
+			__("NZCF CadetNet - Ranks", 'nzcf-cadet-net'), // Page title
+			__("Ranks", 'nzcf-cadet-net'),       // Menu text
+			"manage_options", // Req capability 
+			"cadet_net_menu",  // Menu slug. First one should be identical
+			"cadetnet_admin_menu_ranks"
+		);
+	}
+
+	function cadetnet_admin_menu() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo "<h2>".__('Test Settings','nzcf-cadet-net')."</h2>";
+	}
+	
+	
+	function cadetnet_admin_menu_ranks() {
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo "<h3>".__('Ranks','nzcf-cadet-net')."</h3>";
+	}
 	
