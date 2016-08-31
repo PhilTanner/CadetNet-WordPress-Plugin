d7573b94bb2c0444a1ede9ab843b24a8f958d3a8
Phil Tanner <phil@philtanner.com>
2016-08-31 17:44:13 +1200

    Rank edit/insert form complete (needs reordering)

--- admin/admin.php
+++ admin/admin.php
@@ -73,6 +73,80 @@
 		echo '<h2>'.__('Ranks','nzcf-cadet-net').'</h2>';
 		
 		global $wpdb;
+		
+		// Before we extract our data from the DB for display, let's update our records, 
+		// so the DB pull will reflect what's actually in there
+		if( isset($_POST['rank_ids']) && is_array($_POST['rank_ids']) )
+		{
+			foreach( $_POST['rank_ids'] as $rank_id ) {
+				// The nzcf_corps is a bitmask, so calculate it.
+				$nzcf_corps = 0;
+				if( isset($_POST['nzcf_corps_'.$rank_id.'_atc']) && $_POST['nzcf_corps_'.$rank_id.'_atc'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_ATC;
+				} 
+				if( isset($_POST['nzcf_corps_'.$rank_id.'_corps']) && $_POST['nzcf_corps_'.$rank_id.'_corps'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_CORPS;
+				} 
+				if( isset($_POST['nzcf_corps_'.$rank_id.'_sea']) && $_POST['nzcf_corps_'.$rank_id.'_sea'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_SEA;
+				} 
+				// User data handling done by the update function, not our problem.
+				$wpdb->update( 
+					$wpdb->prefix."wpnzcfcn_rank", 
+					array( 
+						'rank' => $_POST['rank_'.$rank_id],
+						'rank_shortname' => $_POST['rank_shortname_'.$rank_id],
+						'ordering' => $_POST['rank_ordering_'.$rank_id],
+						'nzcf20_order' => $_POST['nzcf20_order_'.$rank_id],
+						'nzcf_corps' => $nzcf_corps
+					), 
+					array( 'rank_id' => $rank_id ), 
+					array( 
+						'%s',
+						'%s',
+						'%d',
+						'%d',
+						'%d'
+					), 
+					array( '%d' ) 
+				);
+			}
+			if( strlen(trim($_POST['rank_0'])) && strlen(trim($_POST['rank_shortname_0'])) )
+			{
+				// The nzcf_corps is a bitmask, so calculate it.
+				$nzcf_corps = 0;
+				if( isset($_POST['nzcf_corps_0_atc']) && $_POST['nzcf_corps_0_atc'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_ATC;
+				} 
+				if( isset($_POST['nzcf_corps_0_corps']) && $_POST['nzcf_corps_0_corps'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_CORPS;
+				} 
+				if( isset($_POST['nzcf_corps_0_sea']) && $_POST['nzcf_corps_0_sea'] ) {
+					$nzcf_corps = $nzcf_corps | WPNZCFCN_CADETS_SEA;
+				} 
+				// User data handling done by the update function, not our problem.
+				$wpdb->insert( 
+					$wpdb->prefix."wpnzcfcn_rank", 
+					array( 
+						'rank' => $_POST['rank_0'],
+						'rank_shortname' => $_POST['rank_shortname_0'],
+						'ordering' => $_POST['rank_ordering_0'],
+						'nzcf20_order' => $_POST['nzcf20_order_0'],
+						'nzcf_corps' => $nzcf_corps
+					), 
+					array( 
+						'%s',
+						'%s',
+						'%d',
+						'%d',
+						'%d'
+					)
+				);
+			}
+			echo '<h3>'.__('Saved','nzcf-cadet-net').'</h3>';
+		}
+		
+		
 		$response = $wpdb->get_results( $wpdb->prepare(
 			"
 			SELECT 
@@ -89,40 +163,53 @@
         ) );
  	   
 		?>
-			<table>
-				<thead>	
-					<tr>
-						<th rowspan="2"> <?= __('Rank Name','nzcf-cadet-net') ?> </th>
-						<th rowspan="2"> <?= __('Shortname','nzcf-cadet-net') ?> </th>
-						<th colspan="3"> <?= __('Corps','nzcf-cadet-net') ?> </th>
-						<th rowspan="2"> <?= __('NZCF20 order','nzcf-cadet-net') ?> </th>
-					</tr>
-					<tr>
-						<th> <?= __('Cadet','nzcf-cadet-net') ?> </th>
-						<th> <?= __('ATC','nzcf-cadet-net') ?> </th>
-						<th> <?= __('Sea','nzcf-cadet-net') ?> </th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-						foreach( $response as $rank ) {
+			<form method="post">
+				<table>
+					<thead>	
+						<tr>
+							<th rowspan="2"> <?= __('Rank Name','nzcf-cadet-net') ?> </th>
+							<th rowspan="2"> <?= __('Shortname','nzcf-cadet-net') ?> </th>
+							<th colspan="3"> <?= __('Corps','nzcf-cadet-net') ?> </th>
+							<th rowspan="2"> <?= __('NZCF20 order','nzcf-cadet-net') ?> </th>
+						</tr>
+						<tr>
+							<th> <?= __('Cadet','nzcf-cadet-net') ?> </th>
+							<th> <?= __('ATC','nzcf-cadet-net') ?> </th>
+							<th> <?= __('Sea','nzcf-cadet-net') ?> </th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+							foreach( $response as $rank ) {
+								echo '<tr>';
+								echo '	<td>';
+								echo '		<input type="hidden" name="rank_ids[]" value="'.$rank->rank_id.'" />';
+								echo '		<input type="hidden" name="rank_ordering_'.$rank->rank_id.'" value="'.$rank->ordering.'" />';
+								echo '		<input type="text" name="rank_'.$rank->rank_id.'" id="rank_'.$rank->rank_id.'" value="'.htmlentities($rank->rank).'" maxlength="70" />';
+								echo '	</td>';
+								echo '	<td> <input type="text" name="rank_shortname_'.$rank->rank_id.'" id="rank_shortname_'.$rank->rank_id.'" value="'.htmlentities($rank->rank_shortname).'" maxlength="10" /> ';
+								echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_'.$rank->rank_id.'_corps" id="nzcf_corps_'.$rank->rank_id.'_corps" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_CORPS?' checked="checked"':'').' /> ';
+								echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_'.$rank->rank_id.'_atc" id="nzcf_corps_'.$rank->rank_id.'_atc" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_ATC?' checked="checked"':'').' /> ';
+								echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_'.$rank->rank_id.'_sea" id="nzcf_corps_'.$rank->rank_id.'_sea" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_SEA?' checked="checked"':'').' /> ';
+								echo '	<td> <input type="number" name="nzcf20_order_'.$rank->rank_id.'" id="nzcf20_order_'.$rank->rank_id.'" value="'.$rank->nzcf20_order.'" /> ';	
+								echo '</tr>';
+							}
 							echo '<tr>';
 							echo '	<td>';
-							echo '		<input type="hidden" name="rank_ids[]" value="'.$rank->id.'" />';
-							echo '		<input type="hidden" name="rank_ordering_'.$rank->id.'" value="'.$rank->ordering.'" />';
-							echo '		<input type="text" name="rank_'.$rank->id.'" id="rank_'.$rank->id.'" value="'.htmlentities($rank->rank).'" maxlength="70" />';
+							echo '		<input type="hidden" name="rank_ordering_0" value="1000" />';
+							echo '		<input type="text" name="rank_0" id="rank_0" value="" maxlength="70" />';
 							echo '	</td>';
-							echo '	<td> <input type="text" name="rank_shortname_'.$rank->id.'" id="rank_shortname_'.$rank->id.'" value="'.htmlentities($rank->rank_shortname).'" maxlength="10" /> ';
-							echo '	<td> <input type="checkbox" name="nzcf_corps_'.$rank->id.'_corps" id="nzcf_corps_'.$rank->id.'_corps" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_CORPS?' checked="checked"':'').' /> ';
-							echo '	<td> <input type="checkbox" name="nzcf_corps_'.$rank->id.'_atc" id="nzcf_corps_'.$rank->id.'_atc" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_ATC?' checked="checked"':'').' /> ';
-							echo '	<td> <input type="checkbox" name="nzcf_corps_'.$rank->id.'_sea" id="nzcf_corps_'.$rank->id.'_sea" value="1" '.($rank->nzcf_corps&WPNZCFCN_CADETS_SEA?' checked="checked"':'').' /> ';
-							echo '	<td> <input type="number" name="nzcf20_order_'.$rank->id.'" id="nzcf20_order_'.$rank->id.'" value="'.$rank->nzcf20_order.'" /> ';
+							echo '	<td> <input type="text" name="rank_shortname_0" id="rank_shortname_0" value="" maxlength="10" /> ';
+							echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_0_corps" id="nzcf_corps_0_corps" value="1" /> ';
+							echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_0_atc" id="nzcf_corps_0_atc" value="1" /> ';
+							echo '	<td align="center"> <input type="checkbox" name="nzcf_corps_0_sea" id="nzcf_corps_0_sea" value="1" /> ';
+							echo '	<td> <input type="number" name="nzcf20_order_0" id="nzcf20_order_0" value="100" /> ';
 							echo '</tr>';
-							
-						}
-					?>
-				</tbody>
-			</table>
+						?>
+					</tbody>
+				</table>
+				<button type="submit"><?= __('Save Changes','nzcf-cadet-net') ?></button>
+			</form>
 		<?php
 	}
 	
